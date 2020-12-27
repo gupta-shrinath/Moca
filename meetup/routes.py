2e102fbf50fb0720637455336e6b065790b0e3fd
Gupta Shrinath <guptashrinath9@gmail.com>
2020-12-27 21:26:42 +0530

    Create function to retreive javascript script
    -

--- meetup/routes.py
+++ meetup/routes.py
@@ -3,4 +3,9 @@ from meetup.models import Meetup
 
 @app.route('/meetup/kotlin/')
 def get_kotlin_events():
-    return Meetup().get_meetups('kotlin-mumbai')
+    return Meetup().get_meetups('kotlin-mumbai',100001)
+
+@app.route('/meetup/javascript/')
+def get_javascript_events():
+    return Meetup().get_paramter_meetups('build-with-code-los-angeles','JavaScript',100002)
+

90f5269a06e489e57756f06fed6aabcc7f2968fc
Gupta Shrinath <guptashrinath9@gmail.com>
2020-12-27 21:32:04 +0530

    Refactor - Remove conversion of timezones - Fetch Organizer image url - Fix to get eventbrite

--- galvanize/models.py
+++ galvanize/models.py
@@ -23,14 +23,14 @@ class Galvanize:
                 links = result.contents[0]['href']
                 if "eventbrite" in links:
                     eventbrite_events.append(links)
-
-                if not result.previous_sibling:
-                    pass
-                else :
-                    topic_list = result.previous_sibling.find_all('p',{'class':'lead'})
-                    for topic in topic_list:
-                        print(topic.getText())
-                        event_topic.append(topic.getText())
+                    if not result.previous_sibling:
+                        pass
+                    else :
+                        topic_list = result.previous_sibling.find_all('p',{'class':'lead'})
+                        for topic in topic_list:
+                            event_topic.append(topic.getText())
+        print(len(eventbrite_events))
+        print(len(event_topic))
         return eventbrite_events,event_topic     
 
     # Get id of all eventbrite url
@@ -39,21 +39,8 @@ class Galvanize:
         event_ids = []
         for event in event_urls:
             substring = event.split("-")
-            print(substring[-1])
             event_ids.append(substring[-1])
         return event_ids
-
-    @staticmethod
-    def get_date_in_indian_timezone(date):
-        america_time_zone = pytz.timezone("America/Los_Angeles")
-        india_time_zone = pytz.timezone("Asia/Kolkata")
-        date = datetime.datetime.strptime(date,"%Y-%m-%d %H:%M:%S")
-        date = america_time_zone.localize(date)
-        date = date.astimezone(india_time_zone)
-        date = date.strftime("%Y-%m-%d %H:%M:%S")
-        new_date = date.split(" ")
-        print(new_date[0])
-        return new_date
 
     @staticmethod
     def get_events_details():
@@ -61,30 +48,38 @@ class Galvanize:
         event_ids = Galvanize.get_eventbrite_event_ids(urls)
         topic_index = 0
         for event_id in event_ids:
-            url = 'https://www.eventbriteapi.com/v3/events/'+event_id+'/'
+            url = 'https://www.eventbriteapi.com/v3/events/'+event_id+'/?expand=organizer'
             PERSONAL_OAUTH_TOKEN = "W43MZVDX2EYFUZENUUHX"
             header = {'Authorization': 'Bearer ' + PERSONAL_OAUTH_TOKEN}
             response = requests.get(url,headers=header)
             response = response.json()
-            print(response['start'])
-            response_start_date = response['start']['local']
-            response_start_date = response_start_date.replace("T"," ")
-            start_date = Galvanize.get_date_in_indian_timezone(response_start_date)[0]
-            start_time = Galvanize.get_date_in_indian_timezone(response_start_date)[1]
+            start_date_time = response['start']['local']
+            start_date_time = start_date_time.replace('T',' ')
+            start_date_time = start_date_time.split()
+            organizer_details_url = response['organizer']['resource_uri']
+            organizer_details_response = requests.get(organizer_details_url,headers=header)
+            organizer_details_response = organizer_details_response.json()
             event = {
                     '_id':uuid.uuid4().hex,
                     'name': response['name']['text'],
                     'description':response['description']['text'],
-                    'image':response['logo']['original']['url'],
-                    'date':start_date,
-                    'time':start_time,
-                    'duration':"NA",
+                    #'image':response['logo']['original']['url'],
+                    'date':start_date_time[0],
+                    'time':start_date_time[1],
+                    'timezone':response['start']['timezone'],
+                    #'duration':"NA",
                     'platform_link':response['url'],
-                    'organization':"Galavanize Remote",
-                    'organization_localized_location':response['start']['timezone'],
-                    'rsvp_count': "NULL",
+                    'organization':"Galvanize Remote",
                     'is_online_event':response['online_event'],
+                    #'organization_localized_location':response['start']['timezone'],
+                    #'rsvp_count': "NULL",
             }
+            if organizer_details_response['logo'] is None:
+                event['organization_image'] = 'NA'
+            else:
+                event['organization_image'] = organizer_details_response['logo']['original']['url']
+
+
             if topics[topic_index] == 'Data Science':
                 event['topic_id'] = 100003
             if topics[topic_index] == 'Web Development':

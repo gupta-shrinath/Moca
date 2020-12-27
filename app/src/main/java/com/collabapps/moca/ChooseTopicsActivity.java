a1ba8419f24d4a4a77df943dd91bcfc08b8b37c4
Danyl Fernandes <androidan1304@gmail.com>
2020-12-27 10:36:10 -0800

    Create ChooseTopicsActivity

---
+++ app/src/main/java/com/collabapps/moca/ChooseTopicsActivity.java
@@ -0,0 +1,14 @@
+package com.collabapps.moca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseTopicsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_topics);
+    }
+}

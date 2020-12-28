7955ecaa86f191a1f072e3774713e3e172ed77eb
Anshika Gupta <63110987+Anshika66-Gupta@users.noreply.github.com>
2020-12-28 10:55:22 +0530

    Add files via upload

---
+++ gs.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.girlscript.tech/events"
+html = requests.get(url)
+
+print(html)
+
+soup = BeautifulSoup(html.content, 'html.parser')
+
+reg_links = soup.find_all("a", class_="theme-btn btn-style-two btn-sm")[:3]
+reg_links = [ link["href"] for link in reg_links ]
+
+titles = soup.find_all("h4")[:3]
+titles = [ title.text for title in titles]
+
+date_time = soup.find_all("li", class_="theme_color")[:3]
+date_time = [ dt.text for dt in date_time]
